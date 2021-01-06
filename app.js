eb95a19b9e5fd27f2b39aeb8ce61bd030db90dca
Ben <bbryant@truecoders.io>
2021-01-06 16:25:34 -0600

    Added exercise answers

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(i, "FIZZBUZZ");
+  } else if (i % 3 == 0) {
+    console.log(i, "FIZZ");
+  } else if (i % 5 == 0) {
+    console.log(i, "BUZZ");
+  }
+}
